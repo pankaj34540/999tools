6a1ff029159827c51601bdc76e4d50470b4d7fd5
PANKAJ <128164127+pankaj34540@users.noreply.github.com>
2026-09-19 18:05:27 +0530

    Add legal pages to routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { OwnerPage } from './pages/OwnerPage';
 import { AdminPage } from './pages/AdminPage';
 import { NotificationToast } from './components/common/NotificationToast';
 import { InstallPrompt } from './components/common/InstallPrompt';
+import PrivacyPage from './pages/legal/PrivacyPage';
+import TermsPage from './pages/legal/TermsPage';
+import RefundPage from './pages/legal/RefundPage';
+import ContactPage from './pages/legal/ContactPage';
 
 export default function App() {
   return (
@@ -17,6 +21,10 @@ export default function App() {
           <Route path="/" element={<PublicPage />} />
           <Route path="/owner" element={<OwnerPage />} />
           <Route path="/admin" element={<AdminPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/refund" element={<RefundPage />} />
+          <Route path="/contact" element={<ContactPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
