1ccbdfa316f93b51ffcc1d68634ba843d85aa8cc
PANKAJ <128164127+pankaj34540@users.noreply.github.com>
2026-09-19 17:12:25 +0530

    Add InstallPrompt component to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { PublicPage } from './pages/PublicPage';
 import { OwnerPage } from './pages/OwnerPage';
 import { AdminPage } from './pages/AdminPage';
 import { NotificationToast } from './components/common/NotificationToast';
+import { InstallPrompt } from './components/common/InstallPrompt';
 
 export default function App() {
   return (
     <AppProvider>
       <BrowserRouter>
         <NotificationToast />
+        <InstallPrompt />
         <Routes>
           <Route path="/" element={<PublicPage />} />
           <Route path="/owner" element={<OwnerPage />} />
