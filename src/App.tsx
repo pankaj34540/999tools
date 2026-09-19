b6ccc58595f429dfd4e7533e95d572ec2b835075
PANKAJ <128164127+pankaj34540@users.noreply.github.com>
2026-09-19 18:01:29 +0530

    Refactor App component and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
-import InstallPrompt from './components/common/InstallPrompt';
-import NotificationToast from './components/common/NotificationToast';
-
 import { PublicPage } from './pages/PublicPage';
 import { OwnerPage } from './pages/OwnerPage';
 import { AdminPage } from './pages/AdminPage';
-import PrivacyPage from './pages/legal/PrivacyPage';
-import TermsPage from './pages/legal/TermsPage';
-import RefundPage from './pages/legal/RefundPage';
-import ContactPage from './pages/legal/ContactPage';
+import { NotificationToast } from './components/common/NotificationToast';
+import { InstallPrompt } from './components/common/InstallPrompt';
 
-const App: React.FC = () => {
+export default function App() {
   return (
     <AppProvider>
-      <Router>
+      <BrowserRouter>
+        <NotificationToast />
+        <InstallPrompt />
         <Routes>
           <Route path="/" element={<PublicPage />} />
           <Route path="/owner" element={<OwnerPage />} />
           <Route path="/admin" element={<AdminPage />} />
-          <Route path="/privacy" element={<PrivacyPage />} />
-          <Route path="/terms" element={<TermsPage />} />
-          <Route path="/refund" element={<RefundPage />} />
-          <Route path="/contact" element={<ContactPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-        <InstallPrompt />
-        <NotificationToast />
-      </Router>
+      </BrowserRouter>
     </AppProvider>
   );
-};
-
-export default App;
+}
